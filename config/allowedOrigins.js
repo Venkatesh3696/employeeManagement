826012885fa26f707a8dea904df57e7d2f439efa
Venkatesh3696 <venkateshpentakota888@gmail.com>
2024-04-03 11:56:13 +0530

    added vercel.json

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,9 @@ const allowedOrigins = [
   "https://www.yoursite.com",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
+  "https://employee-management-amber.vercel.app/",
+  "https://employee-management-git-main-venkateshs-projects-e6277b52.vercel.app/",
+  "https://employee-management-jmefc95v9-venkateshs-projects-e6277b52.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
